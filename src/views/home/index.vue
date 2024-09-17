fc14b580b1f479054c4e211d79510b45620e3fbf
lzh <14905029+stained-with-dust@user.noreply.gitee.com>
2024-09-17 21:47:55 +0800

    完成首页二级分类 :sparkles:

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,7 +2,16 @@
     <div class="home">
         <!-- 轮播图 -->
         <div class="block">
-            <div class="product-category-menu"></div>
+            <div class="product-category-menu">
+                <ul v-for="item in categoryList" :key="item.id">
+                    <div>{{ item.cateName }}</div>
+                    <li>
+                        <div @click="tocateGory(items)" v-for="items in item.children" :key="items.id">{{ items.cateName
+                            }}
+                        </div>
+                    </li>
+                </ul>
+            </div>
             <div class="banner">
                 <el-carousel height="470px">
                     <el-carousel-item>
@@ -31,6 +40,23 @@
                 </div>
             </div>
         </div>
+        <!-- 猜你喜欢列表 -->
+        <div class="main-box">
+            <div class="main">
+                <div class="products">
+                    <div class="box-hd">
+                        <div class="title">猜你喜欢</div>
+                        <div class="more">
+                        </div>
+                    </div>
+                    <div class="box-bd">
+                        <div class="list">
+                            <ProductList :productList='LikeList'></ProductList>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <!-- <loading-toast ref="loadingToast"></loading-toast> -->
     </div>
 </template>
@@ -38,11 +64,10 @@
 <script>
 import ProductList from '../../components/base/productList'
 // import LoadingToast from '../../components/canvasShow/components/toast'
-// import { listSearchMixin } from '../../config/mixin'
-// import {mydataview} from './mydataview.vue'
-// import api from '../../api'
+import { listSearchMixin } from '../../config/mixin'
+import api from '../../api'
 export default {
-    // mixins: [listSearchMixin],
+    mixins: [listSearchMixin],
     name: 'index',
     components: { ProductList },
     data() {
@@ -82,39 +107,179 @@ export default {
                     storeInfo: "苹果15",
                     image: "https://bpic.588ku.com/element_origin_min_pic/23/04/25/2a96cb3b448e184dd2f85772b8389bd9.jpg"
                 }
+            ],
+            // 猜你喜欢
+            LikeList: [
+                {
+                    id: 1,
+                    storeName: "Phone15",
+                    price: 999,
+                    otPrice: 888,
+                    storeInfo: "苹果15",
+                    image: "https://bpic.588ku.com/element_origin_min_pic/23/04/25/2a96cb3b448e184dd2f85772b8389bd9.jpg"
+                },
+                {
+                    id: 1,
+                    storeName: "Phone15",
+                    price: 999,
+                    otPrice: 888,
+                    storeInfo: "苹果15",
+                    image: "https://bpic.588ku.com/element_origin_min_pic/23/04/25/2a96cb3b448e184dd2f85772b8389bd9.jpg"
+                },
+                {
+                    id: 1,
+                    storeName: "Phone15",
+                    price: 999,
+                    otPrice: 888,
+                    storeInfo: "苹果15",
+                    image: "https://bpic.588ku.com/element_origin_min_pic/23/04/25/2a96cb3b448e184dd2f85772b8389bd9.jpg"
+                },
+                {
+                    id: 1,
+                    storeName: "Phone15",
+                    price: 999,
+                    otPrice: 888,
+                    storeInfo: "苹果15",
+                    image: "https://bpic.588ku.com/element_origin_min_pic/23/04/25/2a96cb3b448e184dd2f85772b8389bd9.jpg"
+                }
+            ],
+            // 分类列表
+            categoryList: [
+                {
+                    id: 1,
+                    cateName: "手机",
+                    children: [
+                        { id: 101, cateName: "苹果" },
+                        { id: 102, cateName: "三星" },
+                        { id: 103, cateName: "小米" }
+                    ]
+                },
+                {
+                    id: 2,
+                    cateName: "电脑",
+                    children: [
+                        { id: 201, cateName: "联想" },
+                        { id: 202, cateName: "惠普" },
+                        { id: 203, cateName: "华硕" }
+                    ]
+                },
+                {
+                    id: 3,
+                    cateName: "家电",
+                    children: [
+                        { id: 301, cateName: "冰箱" },
+                        { id: 302, cateName: "洗衣机" },
+                        { id: 303, cateName: "空调" }
+                    ]
+                },
+                {
+                    id: 4,
+                    cateName: "耳机",
+                    children: [
+                        { id: 401, cateName: "蓝牙耳机" },
+                        { id: 402, cateName: "有线耳机" }
+                    ]
+                }
             ]
         }
     },
     mounted() {
         this.getIndexBast()
+        this.getHomeCategory()
+        this.getHomeGuessyourLike()
+        this.getHomeBanner()
     },
     methods: {
         // 获取首页精品推荐
         getIndexBast() {
             // this.$refs.loadingToast.setCanvasLoading(true)
-            // var _this = this
-            // let params = {
-            //     url: api.getIndexBast,
-            //     method: 'GET'
-            // }
-            // this.sendReq(params, (res) => {
-            //     if (res.status === 200) {
-            //         this.$refs.loadingToast.setCanvasLoading(false)
-            //         this.BastList = res.data.splice(0, 4)
-            //     } else {
-            //         _this.$message({
-            //             message: res.msg,
-            //             type: 'warning'
-            //         })
-            //     }
-            // })
+            var _this = this
+            let params = {
+                url: api.getIndexBast,
+                method: 'GET'
+            }
+            this.sendReq(params, (res) => {
+                if (res.status === 200) {
+                    // this.$refs.loadingToast.setCanvasLoading(false)
+                    this.BastList = res.data.splice(0, 4)
+                } else {
+                    _this.$message({
+                        message: res.msg,
+                        type: 'warning'
+                    })
+                }
+            })
         },
-        // 获取我的数据
-
+        // 获取分类列表
+        getHomeCategory() {
+            // this.$refs.loadingToast.setCanvasLoading(true)
+            var _this = this
+            let params = {
+                url: api.getCategory,
+                method: 'GET'
+            }
+            this.sendReq(params, (res) => {
+                if (res.status === 200) {
+                    // this.$refs.loadingToast.setCanvasLoading(false)
+                    this.categoryList = res.data
+                } else {
+                    _this.$message({
+                        message: res.msg,
+                        type: 'warning'
+                    })
+                }
+            })
+        },
+        // 获取猜你喜欢
+        getHomeGuessyourLike() {
+            var _this = this
+            let params = {
+                url: api.getIndexLike,
+                method: 'GET'
+            }
+            this.sendReq(params, (res) => {
+                if (res.status === 200) {
+                    this.LikeList = res.data.splice(0, 4)
+                } else {
+                    _this.$message({
+                        message: res.msg,
+                        type: 'warning'
+                    })
+                }
+            })
+        },
+        // 跳转到分类页
+        tocateGory(item) {
+            this.$router.push({
+                path: '/category',
+                query: {
+                    categoryName: item.cateName,
+                    cid: item.id
+                }
+            })
+        },
+        // 获取首页轮播图
+        getHomeBanner() {
+            var _this = this
+            let params = {
+                url: api.getIndexBanner,
+                method: 'GET'
+            }
+            this.sendReq(params, (res) => {
+                if (res.status === 200) {
+                    this.indexBanner = res.data
+                } else {
+                    _this.$message({
+                        message: res.msg,
+                        type: 'warning'
+                    })
+                }
+            })
+        },
     }
 }
 </script>
-<style scoped>
+<style lang="scss" scoped>
 .home {
     margin: 0 auto;
     width: 1250px;
@@ -142,41 +307,6 @@ export default {
     color: #fff;
     margin-right: 10px;
     float: left;
-}
-
-.product-category-menu ul {
-    padding: 3px 5px;
-}
-
-.product-category-menu ul li {
-    padding: 0 30px;
-    list-style: none;
-}
-
-.product-category-menu ul li a {
-    line-height: 26px;
-    color: #ffffff;
-    font-size: 16px;
-    transition: color 0.3s;
-    text-decoration: none;
-}
-
-.product-category-menu ul li p {
-    padding-left: 18px;
-}
-
-.product-category-menu ul li p a {
-    line-height: 20px;
-    color: #bbbbbb;
-    font-size: 14px;
-    margin-right: 10px;
-    white-space: nowrap;
-    transition: color 0.3s;
-    text-decoration: none;
-}
-
-.product-category-menu ul li p a:hover {
-    color: #FF7800;
 }
 
 .box-hd {
@@ -213,5 +343,43 @@ export default {
 .box-bd .list {
     min-height: 500px;
     padding: 20px 40px;
+}
+
+.product-category-menu {
+    padding: 30px;
+    box-sizing: border-box;
+
+    >ul {
+        &:nth-child(1) {
+            >div {
+                margin-top: 0;
+            }
+        }
+
+        >div {
+            margin: 18px 0;
+        }
+
+        >li {
+            padding-left: 18px;
+            list-style: none;
+            display: flex;
+            flex-wrap: wrap;
+            align-items: center;
+            gap: 5px;
+
+            >div {
+                width: 48%;
+                color: #BBBBBB;
+                font-size: 14px;
+
+                &:hover {
+                    color: #ff6700;
+                    cursor: pointer;
+                }
+            }
+        }
+    }
+
 }
 </style>
